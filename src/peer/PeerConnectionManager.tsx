3bc9c8cd785f1a3011d9803eda34f2290d32fde9
Samuel Cole <scole@homfurniture.com>
2021-01-31 09:55:04 -0600

    Add peerjs and immutablejs, begin integration

---
+++ src/peer/PeerConnectionManager.tsx
@@ -0,0 +1,69 @@
+import Peer from "peerjs";
+import { Map, List } from "immutable";
+
+export default class PeerConnectionManager {
+  static conn?: Peer;
+
+  static peers = Map<string, Peer.DataConnection>();
+
+  private static _dataHandlers = List<(data: any) => void>();
+
+  static register(peerId?: string): Promise<string> {
+    return new Promise((resolve) => {
+      PeerConnectionManager.conn = new Peer(peerId);
+
+      PeerConnectionManager.conn.on("connection", (conn) => {
+        PeerConnectionManager.peers.set(conn.peer, conn);
+        conn.on("data", PeerConnectionManager._handleReceiveData);
+      });
+
+      PeerConnectionManager.conn.on("open", resolve);
+    });
+  }
+
+  static connect(peerId: string): Promise<void> {
+    return new Promise((resolve, reject) => {
+      if (!PeerConnectionManager.conn) {
+        reject("Cannot connect to peer without first calling register.");
+        return;
+      }
+
+      const peerConn = PeerConnectionManager.conn.connect(peerId);
+      PeerConnectionManager.peers.set(peerId, peerConn);
+
+      peerConn.on("data", PeerConnectionManager._handleReceiveData);
+      peerConn.on("open", resolve);
+    });
+  }
+
+  static send(peerId: string, payload: string) {
+    if (!PeerConnectionManager.peers.has(peerId)) {
+      throw new Error(
+        `Cannot send message to ${peerId}, no connection exists.`
+      );
+    }
+    PeerConnectionManager.peers.get(peerId)!.send(payload);
+  }
+
+  // Event handlers
+  static onReceiveData(fn: (data: any) => void) {
+    PeerConnectionManager._dataHandlers.push(fn);
+  }
+
+  static onReceiveConnection(
+    fn: (dataConnection: Peer.DataConnection) => void
+  ) {
+    if (!PeerConnectionManager.conn) {
+      throw new Error(
+        "Cannot handle incoming connections without first calling register."
+      );
+    }
+    PeerConnectionManager.conn.on("connection", fn);
+  }
+
+  private static _handleReceiveData(data: any) {
+    PeerConnectionManager._dataHandlers.forEach((fn) => {
+      fn(data);
+    });
+  }
+}
