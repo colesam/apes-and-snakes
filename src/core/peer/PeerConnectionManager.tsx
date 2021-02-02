58c067dbbdcbf76c2382bd20ebc8c6c5c7ef8699
Samuel Cole <cole.samuel21@gmail.com>
2021-02-02 16:38:37 -0600

    Create basic working version of WebRTC synced flux storage

--- src/core/peer/PeerConnectionManager.tsx
+++ src/core/peer/PeerConnectionManager.tsx
@@ -1,5 +1,9 @@
-import Peer from "peerjs";
+import Peer, { DataConnection } from "peerjs";
 import { Map, List } from "immutable";
+// @ts-ignore
+import { serialize } from "json-immutable/lib/serialize";
+// @ts-ignore
+import { deserialize } from "json-immutable/lib/deserialize";
 
 export default class PeerConnectionManager {
   /**
@@ -16,27 +20,73 @@ export default class PeerConnectionManager {
    * List of functions that are called when data is received.
    * @private
    */
-  private static _dataHandlers = List<(data: any) => void>();
+  private static _dataHandlers = List<(peerId: string, data: any) => void>();
+
+  /**
+   * List of functions that are called when new peer connection is received.
+   * @private
+   */
+  private static _connectionHandlers = List<(conn: DataConnection) => void>();
 
   static register(peerId?: string): Promise<string> {
     return new Promise((resolve, reject) => {
-      PeerConnectionManager.conn = new Peer(peerId);
+      PeerConnectionManager.conn = new Peer(peerId, { debug: 2 });
 
-      PeerConnectionManager.conn.on("connection", (conn) => {
-        console.log(`New connection from peer ${conn.peer}`);
-        PeerConnectionManager.peers = PeerConnectionManager.peers.set(
-          conn.peer,
-          conn
-        );
-        conn.on("data", PeerConnectionManager._handleReceiveData);
-      });
+      PeerConnectionManager.conn.on(
+        "connection",
+        this._handleReceiveConnection
+      );
 
       PeerConnectionManager.conn.on("open", resolve);
-      PeerConnectionManager.conn.on("error", reject);
+      PeerConnectionManager.conn.on("error", (err) => {
+        console.error(err);
+        reject();
+      });
     });
   }
 
-  static connect(peerId: string): Promise<void> {
+  static async connect(peerId: string) {
+    let attempt = 1;
+
+    // Chrome bug causes connections to never trigger correctly, attempt 3 times.
+    while (true) {
+      console.log(
+        `[DEBUG] Establishing connection to ${peerId}. Attempt ${attempt}/3.`
+      );
+
+      try {
+        await PeerConnectionManager._establishConnection(peerId);
+        break;
+      } catch (e) {
+        if (e === "Timeout" && attempt <= 3) {
+          attempt++;
+        } else {
+          throw e;
+        }
+      }
+    }
+
+    if (!PeerConnectionManager.peers.has(peerId)) {
+      throw new Error(`Failed to establish connection to ${peerId}`);
+    }
+  }
+
+  static send(peerId: string, payload: any) {
+    if (!PeerConnectionManager.peers.has(peerId)) {
+      throw new Error(
+        `Cannot send message to ${peerId}, no connection exists.`
+      );
+    }
+    PeerConnectionManager.peers.get(peerId)!.send(serialize(payload));
+  }
+
+  static broadcast(payload: any) {
+    for (const peerId of PeerConnectionManager.peers.keys()) {
+      PeerConnectionManager.send(peerId, payload);
+    }
+  }
+
+  private static _establishConnection(peerId: string): Promise<void> {
     return new Promise((resolve, reject) => {
       if (!PeerConnectionManager.conn) {
         reject("Cannot connect to peer without first calling register.");
@@ -44,27 +94,38 @@ export default class PeerConnectionManager {
       }
 
       const peerConn = PeerConnectionManager.conn.connect(peerId);
-      PeerConnectionManager.peers = PeerConnectionManager.peers.set(
-        peerId,
-        peerConn
-      );
+      const timeoutId = setTimeout(() => {
+        reject("Timeout");
+      }, 5000);
 
-      peerConn.on("data", PeerConnectionManager._handleReceiveData);
-      peerConn.on("open", resolve);
+      peerConn.on("open", () => {
+        clearTimeout(timeoutId);
+
+        if (!PeerConnectionManager.peers.has(peerId)) {
+          console.log(`[DEBUG] Peer connection opened to: ${peerId}`);
+
+          peerConn.on("data", (data) =>
+            PeerConnectionManager._handleReceiveData(peerId, data)
+          );
+
+          PeerConnectionManager.peers = PeerConnectionManager.peers.set(
+            peerId,
+            peerConn
+          );
+
+          resolve();
+        }
+      });
+
+      peerConn.on("error", (err) => {
+        console.error(err);
+        reject(err);
+      });
     });
   }
 
-  static send(peerId: string, payload: string) {
-    if (!PeerConnectionManager.peers.has(peerId)) {
-      throw new Error(
-        `Cannot send message to ${peerId}, no connection exists.`
-      );
-    }
-    PeerConnectionManager.peers.get(peerId)!.send(payload);
-  }
-
   // Event handlers
-  static onReceiveData(fn: (data: any) => void) {
+  static onReceiveData(fn: (peerId: string, data: any) => void) {
     PeerConnectionManager._dataHandlers = PeerConnectionManager._dataHandlers.push(
       fn
     );
@@ -73,17 +134,34 @@ export default class PeerConnectionManager {
   static onReceiveConnection(
     fn: (dataConnection: Peer.DataConnection) => void
   ) {
-    if (!PeerConnectionManager.conn) {
-      throw new Error(
-        "Cannot handle incoming connections without first calling register."
-      );
-    }
-    PeerConnectionManager.conn.on("connection", fn);
+    PeerConnectionManager._connectionHandlers = PeerConnectionManager._connectionHandlers.push(
+      fn
+    );
   }
 
-  private static _handleReceiveData(data: any) {
+  private static _handleReceiveData(peerId: string, data: any) {
+    console.log(`[DEBUG] Received data: ${data}`);
+    console.log(`[DEBUG] From peer: ${peerId}`);
+
     PeerConnectionManager._dataHandlers.forEach((fn) => {
-      fn(data);
+      fn(peerId, deserialize(data));
+    });
+  }
+
+  private static _handleReceiveConnection(conn: DataConnection) {
+    console.log(`[DEBUG] New connection from peer ${conn.peer}`);
+
+    PeerConnectionManager.peers = PeerConnectionManager.peers.set(
+      conn.peer,
+      conn
+    );
+
+    conn.on("data", (data) =>
+      PeerConnectionManager._handleReceiveData(conn.peer, data)
+    );
+
+    PeerConnectionManager._connectionHandlers.forEach((fn) => {
+      fn(conn);
     });
   }
 }
