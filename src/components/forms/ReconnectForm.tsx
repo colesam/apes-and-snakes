d41289ef8fb278e3419ce34601c7d2ef3f2276b1
Samuel Cole <scole@homfurniture.com>
2021-02-06 11:32:45 -0600

    Create reconnection form

---
+++ src/components/forms/ReconnectForm.tsx
@@ -0,0 +1,68 @@
+import {
+  Button,
+  FormControl,
+  FormLabel,
+  HStack,
+  Input,
+  PinInput,
+  PinInputField,
+  Stack,
+} from "@chakra-ui/react";
+import React, { useRef, useState } from "react";
+
+type PropTypes = {
+  initialRoomCode: string;
+  isSubmitting?: boolean;
+  onSubmit?: (roomCode: string) => void;
+};
+
+function ReconnectForm({
+  initialRoomCode,
+  isSubmitting = false,
+  onSubmit,
+}: PropTypes) {
+  const [roomCode, setRoomCode] = useState(initialRoomCode);
+
+  const handleRoomCodeChange = (roomCode: string) => {
+    setRoomCode(roomCode.toUpperCase());
+  };
+
+  const handleSubmit = () => {
+    if (onSubmit) {
+      onSubmit(roomCode);
+    }
+  };
+
+  return (
+    <>
+      <FormControl id="room-code" mb={5}>
+        <FormLabel>Room Code:</FormLabel>
+        <HStack justifyContent="space-between">
+          <PinInput
+            type="alphanumeric"
+            value={roomCode}
+            onChange={handleRoomCodeChange}
+          >
+            <PinInputField />
+            <PinInputField />
+            <PinInputField />
+            <PinInputField />
+          </PinInput>
+        </HStack>
+      </FormControl>
+
+      <Button
+        colorScheme="blue"
+        w="100%"
+        onClick={handleSubmit}
+        isLoading={isSubmitting}
+        loadingText="Connecting"
+        variant={isSubmitting ? "outline" : "solid"}
+      >
+        Reconnect
+      </Button>
+    </>
+  );
+}
+
+export default ReconnectForm;
