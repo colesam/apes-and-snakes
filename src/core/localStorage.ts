cf9711b758d76383ca242702e62e9df719bdb222
Samuel Cole <cole.samuel21@gmail.com>
2021-02-06 07:15:45 -0600

    Get personalKey to initialize in local storage immediately

--- src/core/localStorage.ts
+++ src/core/localStorage.ts
@@ -5,7 +5,7 @@ import { deserialize } from "json-immutable/lib/deserialize";
 import recordTypes from "./store/types/recordTypes";
 
 type Get = (key: string) => any;
-type Set = (key: string, value: any) => void;
+type Set = <T>(key: string, value: T) => T;
 
 const initStorage = (
   type: "localStorage" | "sessionStorage",
@@ -17,13 +17,15 @@ const initStorage = (
     throw new Error(`Failed to initialize window.${type}`);
   }
 
-  const get = (key: string) =>
+  const get: Get = key =>
     deserialize(storageApi.getItem(`${namespace}_${key}`), {
       recordTypes,
     });
 
-  const set = (key: string, value: any) =>
+  const set: Set = (key, value) => {
     storageApi.setItem(`${namespace}_${key}`, serialize(value));
+    return value;
+  };
 
   return [get, set];
 };
