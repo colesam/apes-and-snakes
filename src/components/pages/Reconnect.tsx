c782f5c7c1e481db0ebbf8aae807aced252d8273
Samuel Cole <cole.samuel21@gmail.com>
2021-02-06 11:32:45 -0600

    Create reconnection form

--- src/components/pages/Reconnect.tsx
+++ src/components/pages/Reconnect.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Text } from "@chakra-ui/react";
 import PeerConnectionManager from "../../core/peer/PeerConnectionManager";
 import { Redirect } from "wouter";
@@ -6,41 +6,51 @@ import peerActions from "../../core/peer/peerActions";
 import { useSharedStore } from "../../core/store/sharedStore";
 import { usePrivateStore } from "../../core/store/privateStore";
 import generateId from "../../core/generateId";
+import ReconnectForm from "../forms/ReconnectForm";
+import { namespace } from "../../config";
+import storeActions from "../../core/store/storeActions";
 
 function Reconnect() {
-  const [secretKey, hostPeerId] = usePrivateStore(s => [
-    s.secretKey,
-    s.hostPeerId,
-  ]);
-
+  // State
+  const secretKey = usePrivateStore(s => s.secretKey);
   const roomCode = useSharedStore(s => s.roomCode);
-
-  useEffect(() => {
-    console.log(`[DEBUG] Reconnecting to ${hostPeerId}`);
-    // Register self
-    PeerConnectionManager.register(`${hostPeerId} ${generateId()}`)
-      .then(() => {
-        // Connect to room
-        PeerConnectionManager.connect(hostPeerId)
-          .then(() => {
-            peerActions.ping(hostPeerId);
-            peerActions.reconnect(hostPeerId, secretKey);
-            peerActions.pullShared(hostPeerId);
-          })
-          .catch(err => console.error(err));
-      })
-      .catch(err => console.error(err));
-  }, [hostPeerId]);
+  const [isConnecting, setIsConnecting] = useState(false);
 
   if (roomCode) {
     return <Redirect to="/lobby" />;
   }
 
-  if (!secretKey || !hostPeerId) {
-    return <Redirect to="/" />;
-  }
+  // Handlers
+  const handleConnect = (roomCode: string) => {
+    setIsConnecting(true);
 
-  return <Text>Reconnecting...</Text>;
+    const hostPeerId = `${namespace} ${roomCode}`;
+    const peerId = `${hostPeerId} ${generateId()}`;
+
+    // Register self
+    PeerConnectionManager.register(peerId)
+      .then(() => {
+        // Connect to room
+        PeerConnectionManager.connect(hostPeerId)
+          .then(() => {
+            storeActions.setHostPeerId(hostPeerId);
+            peerActions.ping(hostPeerId);
+            peerActions.reconnect(hostPeerId, secretKey);
+            peerActions.pullShared(hostPeerId);
+          })
+          .catch(err => console.error(err))
+          .finally(() => setIsConnecting(false));
+      })
+      .catch(err => console.error(err));
+  };
+
+  return (
+    <ReconnectForm
+      initialRoomCode={roomCode}
+      isSubmitting={isConnecting}
+      onSubmit={handleConnect}
+    />
+  );
 }
 
 export default Reconnect;
